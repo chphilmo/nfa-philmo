895143a10d4c46954b2e19120be89d524286d5ba
System Administrator <root@macbook-pro-de-phil-2.home>
2023-01-17 16:57:47 +0100

    update api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://dapi.philmo.ch/api",
+  baseURL: "https://dapi.philmo.ch/api",
   headers: {
     "Content-Type": "application/json",
   },
